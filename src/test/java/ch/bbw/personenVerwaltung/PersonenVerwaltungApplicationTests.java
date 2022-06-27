686353d864c05efe5a7aa247c50205dc43aea9fe
Filippo Andretta <filippo.andretta@bluewin.ch>
2022-06-27 12:15:50 +0200

    Initial commit

---
+++ src/test/java/ch/bbw/personenVerwaltung/PersonenVerwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.personenVerwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonenVerwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
